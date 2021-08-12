37be3bda3b387fed01d9bfd355fcf23b21100215
igotodev <gotodev@protonmail.com>
2021-08-12 19:52:33 +0300

    Массивы массивов

--- src/by/godev/algorithm/multi_array/task13/MultiArrayTask13.java
+++ src/by/godev/algorithm/multi_array/task13/MultiArrayTask13.java
@@ -36,16 +36,15 @@ public class MultiArrayTask13 {
 
 	public static void bubbleSort(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
-				if (arr[i] < arr[j]) {
-					int mid = 0;
-					mid = arr[i];
-					arr[i] = arr[j];
+			for (int j = 1; j < arr.length - i; j++) {
+				int mid = 0;
+				if (arr[j-1] > arr[j]) {
+					mid = arr[j - 1];
+					arr[j - 1] = arr[j];
 					arr[j] = mid;
 				}
 			}
 		}
-
 	}
 
 	public static void reverseMatrixColumn(int[][] arr) {
